35ef2133e2c8d2e44b5a90b587d49847b1115d27
surprisedsy <jeh807@gmail.com>
2018-09-21 14:03:14 +0900

    test

--- ex3-4.py
+++ ex3-4.py
@@ -6,9 +6,7 @@ connection = HTTPConnection("www.example.com")
 # 2. 요청 보내기
 connection.request("GET", "/")
 
-# 3. 응답 받기
-response = connection.getresponse()
-print(response.status, response.reason)
+
 
 # 4. Body 읽어오기
 body = response.read()
