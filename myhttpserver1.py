a2b29fabfc2da6cd73b17622b412a148d85f76ff
surprisedsy <jeh807@gmail.com>
2018-09-21 14:45:15 +0900

    myhttpserver1 완성!!!!!!!^ㅇ^

---
+++ myhttpserver1.py
@@ -0,0 +1,30 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+port = 8888
+
+class MyHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/html; charset=utf-8")
+        self.end_headers()                                              # 응답 헤더
+
+        self.wfile.write("<h1>안녕하세요</h1>".encode("utf-8"))          # 응답 바디
+
+
+
+
+
+httpd = HTTPServer(("", port), MyHTTPRequestHandler)
+print("Server running on port", port)
+httpd.serve_forever()
+
+
+
+
+
+
+
+
+
+
+
